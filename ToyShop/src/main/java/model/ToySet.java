a74085c012e026a98b8eacc03a5eb9a117595cfb
BorisLaskin <varislaskin@list.ru>
2024-03-04 00:24:22 +0300

    ToySet_done

--- ToyShop/src/main/java/model/ToySet.java
+++ ToyShop/src/main/java/model/ToySet.java
@@ -2,7 +2,9 @@ package model;
 
 import MyExceptions.ToyExistException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ToySet {
@@ -14,21 +16,37 @@ public class ToySet {
     }
 
     public void addNewToy(String name, ToyType type) {
-        generateNewID();
-        this.toyMap.putIfAbsent(new Toy(this.idCounter, name, type), this.idCounter);
-    }
-
-    public Integer internalAdd(Toy toy) {
-        Integer id = this.toyMap.putIfAbsent(toy, toy.getId());
-        if (id != null) {
-            throw new ToyExistException(this.toyMap.get(toy));
-        }
-        else {
-            return this.toyMap.get(toy);
+        Toy temp = new Toy(1, name, type);
+        if (this.toyMap.containsKey(temp)) {
+            throw new ToyExistException(this.toyMap.get(temp));
+        } else {
+            generateNewID();
+            this.toyMap.putIfAbsent(new Toy(this.idCounter, name, type), this.idCounter);
         }
     }
+    public void addNewToy(Toy toy){
+        this.addNewToy(toy.getName(), toy.getType());
+    }
 
-    public Map<Toy, Integer> getList() {
-        return toyMap;
+    public Map<Toy, Integer> getMap() {
+        return this.toyMap;
+    }
+    public List<Toy> getToyList() {
+        return new ArrayList<Toy>(this.toyMap.keySet());
+    }
+    public Toy getToyById(int id){
+        for (Toy item: this.toyMap.keySet()) {
+            if (item.getId() == id)
+                return item;
+        }
+        return null;
+    }
+    public List<Toy> getToyListByType(ToyType type){
+        List<Toy> list = new ArrayList<>();
+        for (Toy item: this.toyMap.keySet()) {
+            if (item.getType().equals(type))
+                list.add(item);
+        }
+        return list;
     }
 }
